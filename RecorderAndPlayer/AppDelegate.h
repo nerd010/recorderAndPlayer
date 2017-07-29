34687bcd73ef1c258dc5ee69e61decfb9d753313
smallX <xieyu559@163.com>
2017-07-29 12:00:16 +0800

    add files

---
+++ RecorderAndPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecorderAndPlayer
+//
+//  Created by Charles Wang on 16/5/4.
+//  Copyright © 2016年 CHW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecorderAndPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RecorderAndPlayer
+//
+//  Created by Charles Wang on 16/5/4.
+//  Copyright © 2016年 CHW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
